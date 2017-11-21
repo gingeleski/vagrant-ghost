a0fdbe42fffa9ee3229b4d26f7bbf1a4226c1619
Randy Gingeleski <rjg26247@gmail.com>
2017-11-20 20:10:53 -0500

    fix issues with npm in setup.sh via vagrant

--- setup.sh
+++ setup.sh
@@ -12,13 +12,15 @@ sudo apt-get --assume-yes install unzip
 sudo unzip -d /var/www/ghost ghost-latest.zip
 
 # Install NodeJS, npm
-sudo apt-get --assume-yes install nodejs
-sudo ln -s `which nodejs` /usr/bin/node
-sudo apt-get --assume-yes install npm
+#sudo apt-get --assume-yes install nodejs
+#sudo ln -s `which nodejs` /usr/bin/node
+#sudo apt-get --assume-yes install npm
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 # Install Ghost modules via npm
 cd /var/www/ghost
-sudo npm install
+sudo npm install --no-bin-links
 
 # Use example Ghost config as a starting point
 sudo cp config.example.js config.js
