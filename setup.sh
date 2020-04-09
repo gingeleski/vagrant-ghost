50140d05e9f6a773ebe7604d74b28eca4a12c9b5
Randy Gingeleski <rjg26247@gmail.com>
2020-04-09 12:28:31 -0400

    close #3 - using node 12

--- setup.sh
+++ setup.sh
@@ -1,26 +1,17 @@
 # VAGRANT-GHOST SETUP
 
-# Make sure we're in personal folder, then download Ghost
-cd ~
-wget https://ghost.org/zip/ghost-latest.zip
-
 # Make sure existing packages are up-to-date before we install new ones
 sudo apt-get --assume-yes update
-
-# Install unzip then unzip Ghost
-sudo apt-get --assume-yes install unzip
-sudo unzip -d /var/www/ghost ghost-latest.zip
+sudo apt-get --assume-yes upgrade
 
 # Install NodeJS, npm
-#sudo apt-get --assume-yes install nodejs
-#sudo ln -s `which nodejs` /usr/bin/node
-#sudo apt-get --assume-yes install npm
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# Install Ghost modules via npm
-cd /var/www/ghost
-sudo npm install --no-bin-links
+# Get the Ghost package via npm
+npm install ghost-cli@latest -g
 
-# Use example Ghost config as a starting point
-sudo cp config.example.js config.js
+# For the actual Ghost install step we cannot be root - permit 'vagrant' user
+sudo chown vagrant:vagrant /root
+sudo chown vagrant:vagrant /ghost
+su -c "ghost install local" vagrant
