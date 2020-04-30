5ced769a9031b7514443ec3dc499e1d425d77f26
Randy Gingeleski <rjg26247@gmail.com>
2020-04-30 19:10:21 -0400

    close #6 - expanded setup.sh

--- setup.sh
+++ setup.sh
@@ -14,6 +14,14 @@ npm install ghost-cli@latest -g
 # For the actual Ghost install step we cannot be root - permit 'vagrant' user
 su -c "ghost install local" vagrant
 
+# If a directory "content/" exists then we'll swap the new Ghost installation's with it
+if [ -d "/vagrant/content" ] 
+then
+    su -c "ghost stop" vagrant
+    rm -r /home/vagrant/content
+    cp -r /vagrant/content /home/vagrant/content
+fi
+
 # Now make sure Ghost will be served to loopback and have a host-routable address
 su -c "ghost config server.host 0.0.0.0" vagrant
 su -c "ghost config url \"http://$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'):2368/\"" vagrant
